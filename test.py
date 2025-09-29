6595e50e147e5f22160f9b14178cd61c006e7358
SwarajMP <154656748+SwarajMP@users.noreply.github.com>
2025-09-29 22:24:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBAC7q1YfZwXuR3VLYx_fvXOKqkkzwmk78")
+genai.configure(api_key="api_key")
 
 models = genai.list_models()  # returns a list of available models
 for m in models:
